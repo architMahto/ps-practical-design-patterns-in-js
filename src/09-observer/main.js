807bc86bf74238042cb06d2bd3dcbb3b33ae5ec1
Archit Mahto <architmahto@gmail.com>
2018-10-30 22:18:27 -0400

    Created observers

--- src/09-observer/main.js
+++ src/09-observer/main.js
@@ -1,10 +1,17 @@
 import Task from './task';
+import AuditingService from './auditingService';
+import LoggingService from '/loggingService';
+import NotificationService from './notificationService';
 
 console.log('');
 console.log('---------------------------------------');
 console.log('               Observer                ');
 console.log('---------------------------------------');
 console.log('');
+
+const auditingService = new AuditingService();
+const loggingService = new LoggingService();
+const notificationService = new NotificationService();
 
 let task1 = new Task({
 	name: 'Create a demo for constructor',
